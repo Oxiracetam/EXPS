c197ba54ce5ea74f5a0218af80ab50bdf8bd03ae
Oxiracetam <mikko3m@gmail.com>
2023-01-05 01:47:18 -0800

    1

--- TSEMain.lua
+++ TSEMain.lua
@@ -9,17 +9,11 @@ local Tab = Window:Tab({
    Text = "Trade Simulator PWN"
 })
 
---[[
-local Tab2 = Window:Tab({
-   Text = "Misc"
-})
-]]
-
 local Section = Tab:Section({
-   Text = "Automation"
+   Text = "Auto Games"
 })
 
-local Section2 = Tab:Section({
+local SectionR = Tab:Section({
    Text = "Miscellaneous",
    Side = "Right"
 })
@@ -68,47 +62,7 @@ Section:Slider({
    end
 })
 
---[[
-Section:Check({
-   Text = "AutoSnipe",
-   Flag = "AutoSnipe"
-})
-
-Section:Check({
-   Text = "Silent-Aim",
-   Callback = function(bool)
-       warn(bool)
-   end
-})
-
-Section:Dropdown({
-   Text = "Item",
-   List = {"Dominus", "LOL", "Random"},
-   Callback = function(opt)
-       warn(opt)
-   end
-})
-
-Section:Slider({
-   Text = "Price",
-   Minimum = 0,
-   Default = 1,
-   Maximum = 100000000,
-   --Postfix = "%",
-   Callback = function(n)
-       warn(n)
-   end
-})
-
-Section:Button({
-   Text = "Spawn",
-   Callback = function()
-       warn("Settings Reseted.")
-   end
-})
-]] 
-
-Section2:Check({
+SectionR:Check({
    Text = "Anti-AFK",
    Callback = function()
       local vu = game:GetService("VirtualUser")
@@ -121,16 +75,159 @@ Section2:Check({
    end
 })
 
-Section2:Button({
+SectionR:Button({
    Text = "Self Destruct",
    Callback = function(destruct)
       warn("What are you talking about?")
    end
 })
 
-Section2:Label({
+SectionR:Label({
    Text = "Status: Undetected",
    Color = Color3.fromRGB(100, 190, 31)
 })
 
 Tab:Select()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--[[
+   local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Rain-Design/PPHUD/main/Library.lua'))()
+local Flags = Library.Flags
+
+local Window = Library:Window({
+   Text = "Baseplate"
+})
+
+local Tab = Window:Tab({
+   Text = "Aiming"
+})
+
+local Tab2 = Window:Tab({
+   Text = "Visual"
+})
+
+local Section = Tab:Section({
+   Text = "Aiming"
+})
+
+local Section2 = Tab:Section({
+   Text = "Anti-Aim"
+})
+
+local Section3 = Tab:Section({
+   Text = "Ragebot",
+   Side = "Right"
+})
+
+Section:Check({
+   Text = "Aimbot",
+   Flag = "Aimbot"
+})
+
+Section:Check({
+   Text = "Silent-Aim",
+   Callback = function(bool)
+       warn(bool)
+   end
+})
+
+Section:Dropdown({
+   Text = "Body Part",
+   List = {"Head", "Torso", "Random"},
+   Callback = function(opt)
+       warn(opt)
+   end
+})
+
+Section:Slider({
+   Text = "Hit Chance",
+   Minimum = 0,
+   Default = 60,
+   Maximum = 100,
+   Postfix = "%",
+   Callback = function(n)
+       warn(n)
+   end
+})
+
+Section:Button({
+   Text = "Spawn",
+   Callback = function()
+       warn("Settings Reseted.")
+   end
+})
+
+Section2:Check({
+   Text = "Spin"
+})
+
+Section2:Slider({
+   Text = "Pitch Offset",
+   Minimum = 100,
+   Default = 150,
+   Maximum = 500,
+   Callback = function(n)
+       warn(n)
+   end
+})
+
+Section2:Slider({
+   Text = "Yaw Offset",
+   Minimum = 100,
+   Default = 150,
+   Maximum = 500,
+   Callback = function(n)
+       warn(n)
+   end
+})
+
+Section2:Button({
+   Text = "Resolve Positions"
+})
+
+Section3:Check({
+   Text = "Auto-Wall",
+   Callback = function(bool)
+       warn(bool)
+   end
+})
+
+Section3:Check({
+   Text = "Trigger Bot"
+})
+
+Section3:Check({
+   Text = "Insta-Kill"
+})
+
+Section3:Dropdown({
+   Text = "Hitscan Directions",
+   List = {"Left", "Right", "Up", "Down", "All"},
+   Callback = function(opt)
+       warn(opt)
+   end
+})
+
+Section3:Label({
+   Text = "Status: Undetected",
+   Color = Color3.fromRGB(100, 190, 31)
+})
+
+Tab:Select()
+]]
