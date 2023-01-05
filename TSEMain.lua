6b175057d05df5275d9334157440f49830362a30
Oxiracetam <mikko3m@gmail.com>
2023-01-05 03:57:57 -0800

    1

--- TSEMain.lua
+++ TSEMain.lua
@@ -1,10 +1,9 @@
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
-
+local Remotes = game.ReplicatedStorage.Remotes
 local delay = 0
 local rows = 0
 
 local Window = Library:NewWindow("Oxi's Trader")
-
 local General = Window:NewSection("Automation")
 
 General:CreateToggle("Anti Afk", function(value)
@@ -12,8 +11,7 @@ General:CreateToggle("Anti Afk", function(value)
       value = true
    elseif value == true then
       local vu = game:GetService("VirtualUser")
-      local AFKR = game.ReplicatedStorage.Remotes:WaitForChild("AFK")
-      AFKR:Destroy()
+      Remotes:WaitForChild("AFK"):Destroy()
       game:GetService("Players").LocalPlayer.Idled:connect(function()
 	      vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 	      task.wait()
@@ -24,19 +22,16 @@ General:CreateToggle("Anti Afk", function(value)
 end)
 
 General:CreateToggle("Auto Games", function(value)
-   local Tower = game.ReplicatedStorage.Remotes:WaitForChild("Game_Start")
-   local Interact = game.ReplicatedStorage.Remotes:WaitForChild("Game_Interact")
-
+   local Tower = Remotes:WaitForChild("Game_Start")
+   local Interact = Remotes:WaitForChild("Game_Interact")
    while value == true do
       if value == true then
          Tower:InvokeServer("Towers", {amount = 100})
          task.wait(3)
-
          for i = 1, rows do
             Interact:InvokeServer("Towers", "Click", {row = i, val = 1})
             task.wait(0.5)
          end
-
          Interact:InvokeServer("Towers", "Cashout")
          print("Cashed Out!")
          task.wait(delay)
@@ -84,44 +79,5 @@ end)
 KillingCheats:CreateColorPicker("Picker", Color3.new(255, 255, 255), function(value)
    print(value)
 end)
-
-
-
-
-
-
-
-
-
-
-Section:Check({
-   Text = "Auto Game",
-   Callback = function(autogame)
-      local Tower = game.ReplicatedStorage.Remotes:WaitForChild("Game_Start")
-      local Interact = game.ReplicatedStorage.Remotes:WaitForChild("Game_Interact")
-
-      while autogame == true do
-         Tower:InvokeServer("Towers", {amount = 100})
-         task.wait(3)
-
-         for i = 1, rows do
-            Interact:InvokeServer("Towers", "Click", {row = i, val = 1})
-            task.wait(0.1)
-         end
-
-         Interact:InvokeServer("Towers", "Cashout")
-         print("Cashed Out!")
-         task.wait(delay)
-      end
-   end
-})
-
-local vu = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-	vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-	task.wait()
-	vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-   warn("Saved AFK Kick")
-
 
 ]]
